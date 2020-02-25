bc5e1e030fa1a844e7dcce62da6b7e6038489a35
gankai <gangkai666@gmail.com>
2020-02-25 21:37:00 +0800

    :tada:

---
+++ 4.寻找两个有序数组的中位数.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=javascript
+ *
+ * [4] 寻找两个有序数组的中位数
+ *
+ * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/description/
+ *
+ * algorithms
+ * Hard (36.00%)
+ * Likes:    2201
+ * Dislikes: 0
+ * Total Accepted:    146.5K
+ * Total Submissions: 396.8K
+ * Testcase Example:  '[1,3]\n[2]'
+ *
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ * 
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * 
+ * 示例 1:
+ * 
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 
+ * 则中位数是 2.0
+ * 
+ * 
+ * 示例 2:
+ * 
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 
+ * 则中位数是 (2 + 3)/2 = 2.5
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    
+};
+// @lc code=end
+
---
+++ 976.三角形的最大周长.js
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=976 lang=javascript
+ *
+ * [976] 三角形的最大周长
+ *
+ * https://leetcode-cn.com/problems/largest-perimeter-triangle/description/
+ *
+ * algorithms
+ * Easy (54.24%)
+ * Likes:    48
+ * Dislikes: 0
+ * Total Accepted:    12K
+ * Total Submissions: 22K
+ * Testcase Example:  '[2,1,2]'
+ *
+ * 给定由一些正数（代表长度）组成的数组 A，返回由其中三个长度组成的、面积不为零的三角形的最大周长。
+ * 
+ * 如果不能形成任何面积不为零的三角形，返回 0。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：[2,1,2]
+ * 输出：5
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：[1,2,1]
+ * 输出：0
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：[3,2,3,4]
+ * 输出：10
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 输入：[3,6,2,3]
+ * 输出：8
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 3 <= A.length <= 10000
+ * 1 <= A[i] <= 10^6
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var largestPerimeter = function(A) {
+
+};
+// @lc code=end
+
