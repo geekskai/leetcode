7812b4ae28418e943250d7ab7a3ff2c0b97c3254
gankai <kai.gan@langogo.ai>
2020-11-10 13:23:06 +0800

    :sparkles: add 下一个排列

---
+++ 31.下一个排列.js
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=javascript
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode-cn.com/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (35.43%)
+ * Likes:    778
+ * Dislikes: 0
+ * Total Accepted:    107K
+ * Total Submissions: 301.8K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * 
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 
+ * 必须原地修改，只允许使用额外常数空间。
+ * 
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    
+    for(let i = nums.length-1; i >= 0; i--) {
+        if(nums[i] < nums[i+1]) {
+            const large = nextLarge(i);
+            swap(i, large);
+            reverse(i+1);
+            return;
+        }
+    }
+	
+	// If there is no next permutation reverse the arr
+    nums.reverse()
+    
+    function swap(i, j) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+    
+    function reverse(idx) {
+        let start = idx, end = nums.length-1;
+        
+        while(start < end) {
+            swap(start, end);
+            start++;
+            end--;
+        }
+    }
+    
+    function nextLarge(idx) {
+        for(let i = nums.length-1; i > idx; i--) {
+            if(nums[i] > nums[idx]) return i;
+        }
+    }
+};
+// @lc code=end
+
