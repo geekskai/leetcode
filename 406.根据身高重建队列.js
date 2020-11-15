496e2164a6b6f3a2a2dbb41f421af87a68d092c6
gankai <gangkai666@gmail.com>
2020-11-16 00:57:48 +0800

    :fire:

---
+++ 406.根据身高重建队列.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=406 lang=javascript
+ *
+ * [406] 根据身高重建队列
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+var reconstructQueue = function(people) {
+    let res = []
+    people.sort((a, b) => a[0] == b[0] ? a[1] - b[1] : b[0] - a[0])
+    people.forEach(val => {
+        res.splice(val[1], 0, val)
+    })
+    return res
+};
+// @lc code=end
+
