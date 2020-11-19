ba4b8e9d766e86bed8b2778e3b3ac167fe77272d
gankai <mac@macdeMac-mini-2.lan>
2020-11-19 13:33:36 +0800

    🤔 字形变换

---
+++ 12.整数转罗马数字.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=12 lang=javascript
+ *
+ * [12] 整数转罗马数字
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    return Object.entries(map).reduce((result, [letter, n]) => {
+        result += letter.repeat(Math.floor(num / n));
+        num %= n;
+        return result;
+    }, '');
+};
+// @lc code=end
+
---
+++ 6.z-字形变换.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=javascript
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+     // return original string if can't zigzag
+     if (numRows === 1 || s.length < numRows) return s;
+
+     let rows = []
+     let converted = '';
+     let reverse = false;
+     let count = 0
+ 
+     // prepare rows
+     for (let i = 0; i < numRows; i++) rows[i] = [];
+     // reverse the push flow when reaching turning points
+     for (let i = 0; i < s.length; i++) {
+         rows[count].push(s[i]);
+         reverse ? count-- : count++;
+         if (count === numRows - 1 || count === 0) reverse = !reverse;
+     }
+     // put together converted string
+     return rows.reduce((converted, cur) => converted + cur.join(''), '');
+ 
+};
+// @lc code=end
+
