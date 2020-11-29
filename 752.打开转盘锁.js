267291703aac80ff6ad829cc35d21af5135899b0
gankai <gangkai666@gmail.com>
2020-11-30 00:05:58 +0800

    🎨 767

---
+++ 752.打开转盘锁.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=752 lang=javascript
+ *
+ * [752] 打开转盘锁
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} deadends
+ * @param {string} target
+ * @return {number}
+ */
+var openLock = function(deadends, target) {
+
+};
+// @lc code=end
+
