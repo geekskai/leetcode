1fbc6872420239b6aeca5f3e62f3832d4b47b788
gankai <mac@macdeMac-mini-2.local>
2020-12-04 13:17:14 +0800

    🤔 2020-12-4

---
+++ 667.优美的排列-ii.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=667 lang=javascript
+ *
+ * [667] 优美的排列 II
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[]}
+ */
+var constructArray = function(n, k) {
+
+};
+// @lc code=end
+
