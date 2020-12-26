a3cebf537fc4ac506a3e8c373eb0636d7f6bcf8d
gankai <gangkai666@gmail.com>
2020-12-26 11:46:37 +0800

    :fire: 85

---
+++ 906.超级回文数.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=906 lang=javascript
+ *
+ * [906] 超级回文数
+ */
+
+// @lc code=start
+/**
+ * @param {string} L
+ * @param {string} R
+ * @return {number}
+ */
+var superpalindromesInRange = function(L, R) {
+
+};
+// @lc code=end
+
