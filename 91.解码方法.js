e09d44f7682f053c8bc6ce1f7297e7962bf84989
Gan Kai <kai.gan@notta.ai>
2021-02-02 13:28:21 +0800

    ✨feat(424): add 424

---
+++ 91.解码方法.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=91 lang=javascript
+ *
+ * [91] 解码方法
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+
+};
+// @lc code=end
+
