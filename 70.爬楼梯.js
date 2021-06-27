e37828be3cce01cf64e817acd8266abf6469e666
gankai <geeks.kai@gmail.com>
2021-06-27 23:23:09 +0800

    feat: :zap: [70] dp

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -14,29 +14,29 @@
  * Testcase Example:  '2'
  *
  * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
- * 
+ *
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
- * 
+ *
  * 注意：给定 n 是一个正整数。
- * 
+ *
  * 示例 1：
- * 
+ *
  * 输入： 2
  * 输出： 2
  * 解释： 有两种方法可以爬到楼顶。
  * 1.  1 阶 + 1 阶
  * 2.  2 阶
- * 
+ *
  * 示例 2：
- * 
+ *
  * 输入： 3
  * 输出： 3
  * 解释： 有三种方法可以爬到楼顶。
  * 1.  1 阶 + 1 阶 + 1 阶
  * 2.  1 阶 + 2 阶
  * 3.  2 阶 + 1 阶
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -44,17 +44,12 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    let prev = 0;
-    let cur = 1;
-    let temp;
-    
-    for (let i = 0; i < n; i++) {
-        temp = prev;
-        prev = cur;
-        cur += temp; 
-    }
-    return cur;
+var climbStairs = function (n) {
+  if (n < 3) return n;
+  const dp = [1, 1, 2];
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
 };
 // @lc code=end
-
